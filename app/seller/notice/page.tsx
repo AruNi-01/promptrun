d2062d97ea7418c95ada562df69cb1fc1b951cb3
AarynLu <hello@0x3f4.run>
2024-04-08 00:40:56 +0800

    feat: ✨ seller message notic

--- app/seller/notice/page.tsx
+++ app/seller/notice/page.tsx
@@ -23,7 +23,7 @@ export default function SellerNoticePage() {
         className="border rounded-lg shadow bg-gray-800 border-gray-700 flex flex-col items-start w-3/4 h-[600px] overflow-y-scroll scroll-smooth"
       >
         {Array.from({ length: 10 }).map((_, index) => (
-          <div className="flex items-start gap-2.5 mx-3 my-3">
+          <div key={index} className="flex items-start gap-2.5 mx-3 my-3">
             <img className="w-8 h-8 rounded-full" src="/logo.png" alt="Jese image" />
             <div className="flex flex-col gap-1 w-3/4">
               <div className="flex items-center space-x-2 rtl:space-x-reverse">
@@ -38,7 +38,7 @@ export default function SellerNoticePage() {
                 >
                   <p className="text-sm font-normal text-start">
                     {" "}
-                    That's awesome. I think our users will really appreciate the improvements.
+                    That&aposs awesome. I think our users will really appreciate the improvements.
                   </p>
                 </div>
                 <Checkbox
