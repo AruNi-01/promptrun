179fa4e9fd52bc39f7ba3d8d9ecceb624f236ac6
AarynLu <hello@0x3f4.run>
2024-04-15 23:27:17 +0800

    feat: ✨ add icp no

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,16 +4,19 @@ import Link from "./ui/Link";
 
 export const Footer = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("flex flex-col gap-3 mb-3 mt-10", className)}>
-      <Divider className="mx-auto w-9/12" />
+    <div className={cn("flex flex-col gap-1 mb-3 mt-10", className)}>
+      <Divider className="mx-auto w-9/12 mb-2" />
+      <div className="flex gap-1 mx-auto text-medium text-[#71717A]">
+        <p>Made By</p>
+        <Link href="https://0x3f4.run">AarynLu</Link>
+      </div>
       <div className={cn("mx-auto text-medium text-[#71717A]", "flex gap-2")}>
         <div className="flex">
           <p className="font-normal mr-1">Copyright © {new Date().getFullYear()} </p>
           <Link href="https://github.com/aruni-01/promptrun">PromptRun</Link>.
         </div>
         <div className="flex gap-1">
-          <p>Made By</p>
-          <Link href="https://0x3f4.run">AarynLu</Link>
+          <Link href="https://beian.miit.gov.cn/">黔ICP备2024026145号-1</Link>
         </div>
       </div>
     </div>
